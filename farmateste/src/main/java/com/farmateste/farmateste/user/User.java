1c8d8a9fcd344b12990a042fb583a193164e67fc
Marcus <mv070900@gmail.com>
2025-04-01 16:26:17 -0300

    Criação da SecurityConfig para remoção da autenticação para csrf e alteração do atributo login para username na classe User

--- farmateste/src/main/java/com/farmateste/farmateste/user/User.java
+++ farmateste/src/main/java/com/farmateste/farmateste/user/User.java
@@ -17,6 +17,6 @@ import jakarta.persistence.Id;
 public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String login;
+    private String username;
     private String password;
 }
